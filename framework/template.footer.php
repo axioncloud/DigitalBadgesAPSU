58d37dd2f22bdb37c787f395763dacca08c9b6bf
Nathaniel Martin <nate6631@gmail.com>
2019-03-20 18:22:08 -0500

    Update template.footer.php
    Modified copyright message

--- framework/template.footer.php
+++ framework/template.footer.php
@@ -1,3 +1,3 @@
 <div class="footer-copyright text-center py-3 bg-light">
-  <a href="https://ioncloud64.com">Copyright &copy; 2018 ioncloud64.com All Rights Reserved.</a>
+  <a href="#">Copyright &copy; 2018 Austin Peay State University All Rights Reserved.</a>
 </div>
